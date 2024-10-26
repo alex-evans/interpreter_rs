891f15ea3e601b3eb75a60c273884f390eecbd1e
Alex Evans <alex.evans.d@gmail.com>
2024-10-26 15:15:59 -0500

    Handle string statements

--- src/lib.rs
+++ src/lib.rs
@@ -4,39 +4,52 @@ pub struct Token {
     token_type: String,
     literal: String,
     line: i32,
-    error: bool,
+    error: String,
 }
 
 impl Token {
     pub fn new(input_text: String, line: i32) -> Token {
-        let token_type = match input_text.as_str() {
-            "(" => "LEFT_PAREN".to_string(),
-            ")" => "RIGHT_PAREN".to_string(),
-            "{" => "LEFT_BRACE".to_string(),
-            "}" => "RIGHT_BRACE".to_string(),
-            "*" => "STAR".to_string(),
-            "." => "DOT".to_string(),
-            "," => "COMMA".to_string(),
-            "+" => "PLUS".to_string(),
-            "-" => "MINUS".to_string(),
-            ";" => "SEMICOLON".to_string(),
-            "==" => "EQUAL_EQUAL".to_string(),
-            "=" => "EQUAL".to_string(),
-            "!" => "BANG".to_string(),
-            "!=" => "BANG_EQUAL".to_string(),
-            "<" => "LESS".to_string(),
-            "<=" => "LESS_EQUAL".to_string(),
-            ">" => "GREATER".to_string(),
-            ">=" => "GREATER_EQUAL".to_string(),
-            "/" => "SLASH".to_string(),
-            _ => "UNKNOWN".to_string(),
+        
+        let mut error = "".to_string();
+        let text = input_text.clone();
+        let mut literal = "null".to_string();
+
+        let token_type = if input_text.starts_with("\"") {
+            if input_text.len() > 1 && input_text.ends_with("\"") {
+                literal = input_text[1..input_text.len()-1].to_string();  // remove the quotes
+                "STRING".to_string()
+            } else {
+                error = "Unterminated string.".to_string();
+                "UNKNOWN".to_string()
+            }
+        } else {
+            match input_text.as_str() {
+                "(" => "LEFT_PAREN".to_string(),
+                ")" => "RIGHT_PAREN".to_string(),
+                "{" => "LEFT_BRACE".to_string(),
+                "}" => "RIGHT_BRACE".to_string(),
+                "*" => "STAR".to_string(),
+                "." => "DOT".to_string(),
+                "," => "COMMA".to_string(),
+                "+" => "PLUS".to_string(),
+                "-" => "MINUS".to_string(),
+                ";" => "SEMICOLON".to_string(),
+                "==" => "EQUAL_EQUAL".to_string(),
+                "=" => "EQUAL".to_string(),
+                "!" => "BANG".to_string(),
+                "!=" => "BANG_EQUAL".to_string(),
+                "<" => "LESS".to_string(),
+                "<=" => "LESS_EQUAL".to_string(),
+                ">" => "GREATER".to_string(),
+                ">=" => "GREATER_EQUAL".to_string(),
+                "/" => "SLASH".to_string(),
+                _ => {
+                    error = format!("Unexpected character: {}", input_text);
+                    "UNKNOWN".to_string()
+                }
+            }
         };
-        let text = input_text;
-        let literal = "null".to_string();
-        let mut error = false;
-        if token_type == "UNKNOWN" {
-            error = true;
-        }
+
         Token {
             text,
             token_type,
@@ -47,8 +60,8 @@ impl Token {
     }
 
     pub fn output(&self) {
-        if self.error {
-            eprintln!("[line {}] Error: Unexpected character: {}", self.line, self.text);
+        if self.error != ""{
+            eprintln!("[line {}] Error: {}", self.line, self.error);
         } else {
             println!("{} {} {}", self.token_type, self.text, self.literal)
         }
@@ -94,6 +107,20 @@ pub fn run(
                     }
                 }
             },
+            ('"', _) => {
+                let mut literal = current_char.to_string();
+                while let Some(c) = chars.next() {
+                    literal.push(c);
+                    if c == '\n' {
+                        line += 1;
+                    }
+                    if c == '"' {
+                        break;
+                    }
+                }
+                let token = Token::new(literal, line);
+                tokens.push(token);
+            },
             (' ', _) | ('\t', _) => {},
             ('\n', _) => {
                 line += 1;
@@ -106,7 +133,7 @@ pub fn run(
     }
     
     for token in tokens {
-        if token.error {
+        if token.error != "" {
             return_code = 65;
         }
         token.output();
