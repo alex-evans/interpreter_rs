305530c85c9b592bc579237baeeafb8ea7ac3b43
Alex Evans <alex.evans.d@gmail.com>
2024-10-27 20:56:53 -0500

    Cleaned up code a bit and added number check

--- src/lib.rs
+++ src/lib.rs
@@ -10,44 +10,20 @@ pub struct Token {
 impl Token {
     pub fn new(input_text: String, line: i32) -> Token {
         
-        let mut error = "".to_string();
+        let error;
         let text = input_text.clone();
-        let mut literal = "null".to_string();
+        let literal ;
+        let token_type;
 
         let token_type = if input_text.starts_with("\"") {
-            if input_text.len() > 1 && input_text.ends_with("\"") {
-                literal = input_text[1..input_text.len()-1].to_string();  // remove the quotes
-                "STRING".to_string()
-            } else {
-                error = "Unterminated string.".to_string();
-                "UNKNOWN".to_string()
-            }
+            (token_type, literal, error) = Self::handle_string(input_text);
+            token_type
+        } else if input_text.chars().next().unwrap().is_digit(10) {
+            (token_type, literal, error) = Self::handle_number(input_text);
+            token_type
         } else {
-            match input_text.as_str() {
-                "(" => "LEFT_PAREN".to_string(),
-                ")" => "RIGHT_PAREN".to_string(),
-                "{" => "LEFT_BRACE".to_string(),
-                "}" => "RIGHT_BRACE".to_string(),
-                "*" => "STAR".to_string(),
-                "." => "DOT".to_string(),
-                "," => "COMMA".to_string(),
-                "+" => "PLUS".to_string(),
-                "-" => "MINUS".to_string(),
-                ";" => "SEMICOLON".to_string(),
-                "==" => "EQUAL_EQUAL".to_string(),
-                "=" => "EQUAL".to_string(),
-                "!" => "BANG".to_string(),
-                "!=" => "BANG_EQUAL".to_string(),
-                "<" => "LESS".to_string(),
-                "<=" => "LESS_EQUAL".to_string(),
-                ">" => "GREATER".to_string(),
-                ">=" => "GREATER_EQUAL".to_string(),
-                "/" => "SLASH".to_string(),
-                _ => {
-                    error = format!("Unexpected character: {}", input_text);
-                    "UNKNOWN".to_string()
-                }
-            }
+            (token_type, literal, error) = Self::handle_keyword(input_text);
+            token_type
         };
 
         Token {
@@ -57,6 +33,102 @@ impl Token {
             line,
             error,
         }
+    }
+
+    fn handle_string(
+        input_text: String
+    ) -> (String, String, String) {
+        let literal;
+        let error;
+        let token_type;
+
+        if input_text.len() > 1 && input_text.ends_with("\"") {
+            literal = input_text[1..input_text.len()-1].to_string();  // remove the quotes
+            error = "".to_string();
+            token_type = "STRING".to_string();
+        } else {
+            literal = "null".to_string();
+            error = "Unterminated string.".to_string();
+            token_type = "UNKNOWN".to_string();
+        }
+
+        (token_type, literal, error)
+    }
+
+    fn handle_number(
+        input_text: String
+    ) -> (String, String, String) {
+
+        let literal;
+        let error;
+        let token_type;
+
+        let parts: Vec<&str> = input_text.split('.').collect();
+
+        if parts.len() == 1 {
+            literal = format!("{}.0", input_text);
+            token_type = "NUMBER".to_string();
+            error = "".to_string();
+            return (token_type, literal, error);
+        }
+
+        if parts.len() > 2 {
+            literal = "null".to_string();
+            error = "Invalid number.".to_string();
+            token_type = "UNKNOWN".to_string();
+            return (token_type, literal, error);
+        }
+
+        let integer_part = parts[0];
+        let decimal_part = parts[1].trim_end_matches('0');
+
+        if decimal_part.is_empty() {
+            literal = format!("{}.0", integer_part);
+            error = "".to_string();
+            token_type = "NUMBER".to_string();
+            return (token_type, literal, error);
+        }
+
+        literal = format!("{}.{}", integer_part, decimal_part);
+        error = "".to_string();
+        token_type = "NUMBER".to_string();
+        return (token_type, literal, error);
+    
+    }
+
+    fn handle_keyword(
+        input_text: String
+    ) -> (String, String, String) {
+        let literal = "null".to_string();
+        let mut error = "".to_string();
+
+        let token_type = match input_text.as_str() {
+            "(" => "LEFT_PAREN".to_string(),
+            ")" => "RIGHT_PAREN".to_string(),
+            "{" => "LEFT_BRACE".to_string(),
+            "}" => "RIGHT_BRACE".to_string(),
+            "*" => "STAR".to_string(),
+            "." => "DOT".to_string(),
+            "," => "COMMA".to_string(),
+            "+" => "PLUS".to_string(),
+            "-" => "MINUS".to_string(),
+            ";" => "SEMICOLON".to_string(),
+            "==" => "EQUAL_EQUAL".to_string(),
+            "=" => "EQUAL".to_string(),
+            "!" => "BANG".to_string(),
+            "!=" => "BANG_EQUAL".to_string(),
+            "<" => "LESS".to_string(),
+            "<=" => "LESS_EQUAL".to_string(),
+            ">" => "GREATER".to_string(),
+            ">=" => "GREATER_EQUAL".to_string(),
+            "/" => "SLASH".to_string(),
+            _ => {
+                error = format!("Unexpected character: {}", input_text);
+                "UNKNOWN".to_string()
+            }
+        };
+
+        (token_type, literal, error)
     }
 
     pub fn output(&self) {
@@ -124,6 +196,23 @@ pub fn run(
             (' ', _) | ('\t', _) => {},
             ('\n', _) => {
                 line += 1;
+            },
+            (c, _) if c.is_digit(10) => {
+                let mut number = c.to_string();
+                while let Some(&next_char) = chars.peek() {
+                    if next_char == '.' && number.chars().filter(|&c| c == '.').count() > 0 {
+                        break;
+                    }
+
+                    if next_char.is_digit(10) || next_char == '.' {
+                        number.push(next_char);
+                        chars.next();
+                    } else {
+                        break;
+                    }
+                }
+                let token: Token = Token::new(number, line);
+                tokens.push(token);
             },
             (c, _) => {
                 let token = Token::new(c.to_string(), line);
